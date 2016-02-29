8d0271a060942308c2e05861a03a2b760da0dca9
WXB506 <1922686887@qq.com>
2016-02-29 08:09:52 +0800

    static register service

--- app/src/main/java/com/study/brodcasttest/MainActivity.java
+++ app/src/main/java/com/study/brodcasttest/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -42,7 +44,16 @@ public class MainActivity extends Activity {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+            if(networkInfo != null && networkInfo.isAvailable())
+            {
+                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "network is not available", Toast.LENGTH_SHORT).show();
+            }
+
         }
     }
 }
